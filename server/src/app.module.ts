f930caeafee16c2a28506ddd99adfe75c6050692
Marina Soufi <xysamemir@gmail.com>
2021-12-12 13:40:26 +0100

    add graphql

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -14,6 +16,9 @@ import { AppService } from './app.service';
       database: 'zonnestr',
       entities: ['dist/**/*.entity{.ts,.js}', __dirname + '/entities/**/*.js'],
       synchronize: false,
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/graphql-schema.gql'),
     }),
   ],
   controllers: [AppController],

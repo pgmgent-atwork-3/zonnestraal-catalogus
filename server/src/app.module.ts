f63fdfd05bebbf1b6cf84629331fe1845df08295
Marina Soufi <xysamemir@gmail.com>
2022-01-04 22:40:31 +0100

    update

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -43,6 +43,7 @@ import { LibraryReservationDateModule } from './library-reservation-date/library
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/graphql-schema.gql'),
       context: ({ req }) => ({ headers: req.headers }),
+      
     }),
     LibraryModule,
     LibraryReservationModule,

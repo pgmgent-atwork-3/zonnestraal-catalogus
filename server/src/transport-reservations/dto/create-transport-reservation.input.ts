247e49a4775ab958dc97b8f07972c509b7340308
Marina Soufi <xysamemir@gmail.com>
2022-01-10 15:18:11 +0100

    create car reservation - fixes #53

--- server/src/transport-reservations/dto/create-transport-reservation.input.ts
+++ server/src/transport-reservations/dto/create-transport-reservation.input.ts
@@ -1,7 +1,19 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsInt, IsString } from 'class-validator';
 
 @InputType()
 export class CreateTransportReservationInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @IsInt()
+  @Field(() => Int)
+  transport_id: number;
+
+  @IsString()
+  @Field()
+  name: string;
+
+  @Field()
+  from_date: Date;
+
+  @Field()
+  till_date: Date;
 }
